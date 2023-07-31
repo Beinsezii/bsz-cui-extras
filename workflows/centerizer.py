0b325133e6c28b9a4f3ec3d0e69c7fe52813974a
Beinsezii <beinsezii@gmail.com>
2023-07-31 00:15:01 -0700

    centerizer.py support groups, snap control flag

--- workflows/centerizer.py
+++ workflows/centerizer.py
@@ -19,24 +19,35 @@ def roundint(n: int, step: int) -> int:
 parser = ArgumentParser()
 parser.add_argument('-x', default=600, help="Origin point X. Default 600")
 parser.add_argument('-y', default=600, help="Origin point Y. Default 600")
+parser.add_argument('-s', '--snap', default=10, help="Snap size")
 parser.add_argument('files', nargs='+', type=FileType('r+'), help="List of .json files to centerize")
 
 args = parser.parse_args()
 
 for f in args.files:
     data = json.load(f)
+    snap = int(args.snap)
     minx, miny, maxx, maxy = 99999, 99999, -99999, -99999
     # 1st pass calc bounds
     for node in data["nodes"]:
         x, y = node["pos"]
         minx, miny, maxx, maxy = min(x, minx), min(y, miny), max(x, maxx), max(y, maxy)
 
+    for group in data["groups"]:
+        x, y = group["bounding"][:2]
+        minx, miny, maxx, maxy = min(x, minx), min(y, miny), max(x, maxx), max(y, maxy)
+
     offset_x, offset_y = args.x - (maxx + minx) / 2, args.y - (maxy + miny) / 2
 
     for node in data["nodes"]:
         x, y = node["pos"]
-        node["pos"][0] = int(roundint(x + offset_x, 10))
-        node["pos"][1] = int(roundint(y + offset_y, 10))
+        node["pos"][0] = int(roundint(x + offset_x, snap))
+        node["pos"][1] = int(roundint(y + offset_y, snap))
+
+    for group in data["groups"]:
+        x, y = group["bounding"][:2]
+        group["bounding"][0] = int(roundint(x + offset_x, snap))
+        group["bounding"][1] = int(roundint(y + offset_y, snap))
 
     f.seek(0)
     json.dump(data, f, indent=2)
